f484901d1c76d7ca0cdf944c6123a64efb1fed59
NBATHEAU <Makingvideoooo@gmail.com>
2024-10-31 17:37:21 +0000

    My report

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    chars_dict = count_characters(text)
-    print(chars_dict)  
+    chars_dict = get_chars_dict(text)
+    
+    print("--- Begin report of book/frankenstein.txt ---")
+    print(f"{num_words} worlds found in the document")
+    print ()
+    alpha_chars = [chars for chars in chars_dict if chars.isalpha()]
+    for chars in alpha_chars:
+        numm = chars_dict[chars]
+            
+        print (f"The {chars} character was found {numm} times")
+
+    print("--- End report ---")
+
+
 
 
 
