b581602e6ad7608467e3546716e8e023678d493d
NBATHEAU <Makingvideoooo@gmail.com>
2024-10-31 16:41:52 +0000

    this counting the characters

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    counting_characters = count_characters(text)
+    print (count_characters(text))
+
+def count_characters(text):
+    my_string = text
+    lowered_string = my_string.lower()
+    number_characters = {}
+    for characters in lowered_string:
+        if characters in number_characters :
+            number_characters[characters] += 1
+        else:
+            number_characters[characters] = 1
+    return number_characters
+        
 
 
 def get_num_words(text):
