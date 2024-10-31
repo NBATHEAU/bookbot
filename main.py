87107152e7699b0a9e0786306941fc7074af567c
NBATHEAU <Makingvideoooo@gmail.com>
2024-10-31 15:58:17 +0000

    Goated solution

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-def count_words(text):
-    message = text.split()
-    number_words = len(message)
-    print(number_words)
-    
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count_words(text)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
 
 def get_book_text(path):
     with open(path) as f:
